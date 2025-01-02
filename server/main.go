e46b3a4f48256b020ad7d87203826576cb2c11bc
vincentvnoord <vannoord.vincent@gmail.com>
2025-01-02 17:56:30 +0100

    Server env and gitignore

--- server/main.go
+++ server/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/vincentvnoord/recipe-app/api/handlers"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/", handlers.ScrapeRecipeHandler)
-	fmt.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting server on :%s", port))
+	http.ListenAndServe(":"+port, nil)
 }
